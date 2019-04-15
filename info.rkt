12dc5f299e4f2946b414130fac503110ed0b256d
Oran Simhony <oran.simhony@gmail.com>
2019-04-15 15:22:35 +0300

    Added an info.rkt and basic docs

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "lambda-calculus")
+(define deps '("base" "brag"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/lambda-calculus.scrbl" ())))
+(define pkg-desc "A lambda-calculus DSL in Racket")
+(define version "0.0")
+(define pkg-authors '(oransimhony))
---
+++ scribblings/lambda-calculus.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[lambda-calculus
+                    racket/base]]
+
+@title{lambda-calculus}
+@author{oransimhony}
+
+@defmodule[lambda-calculus]
+
+A lambda-calculus DSL in Racket
