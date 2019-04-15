0ab5385e374699650cfeff8c73df27c081a6f4e3
Oran Simhony <oran.simhony@gmail.com>
2019-04-15 15:29:46 +0300

    Updated dependencies

--- info.rkt
+++ info.rkt
@@ -1,8 +1,10 @@
 #lang info
 (define collection "lambda-calculus")
-(define deps '("base" "brag"))
+(define deps '("beautiful-racket-lib"
+               "rackunit-lib"
+               "base" "brag" "beautiful-racket"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/lambda-calculus.scrbl" ())))
 (define pkg-desc "A lambda-calculus DSL in Racket")
-(define version "0.0")
+(define version "1.0.0")
 (define pkg-authors '(oransimhony))
