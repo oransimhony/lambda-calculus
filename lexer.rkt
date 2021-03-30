649da27e52209b4d94c6eccb9a96748af6293208
Oran Simhony <oran.simhony@gmail.com>
2021-03-30 18:49:49 +0300

    Updated lexing of identifier to support digits and some symbols

--- lexer.rkt
+++ lexer.rkt
@@ -2,7 +2,10 @@
 (require brag/support)
 
 (define-lex-abbrev digits (:+ (char-set "0123456789")))
-(define-lex-abbrev alphabet (:+ (char-set "abcdefghijklmnopqrstuvwxyz")))
+(define-lex-abbrev alphabet (char-set "abcdefghijklmnopqrstuvwxyz"))
+(define-lex-abbrev ident-symbols (char-set "-_*"))
+(define-lex-abbrev ident-chars (:or alphabet ident-symbols))
+(define-lex-abbrev identifier (:seq (:+ ident-chars) (:* (:or ident-chars digits))))
 
 (define-lex-abbrev reserved-terms (:or "(" ")" "λ" "." "=" "lambda"))
 
@@ -14,6 +17,6 @@
    [(from/stop-before ";" "\n") (token lexeme #:skip? #t)]
    [digits (token 'NUM (string->number lexeme))]
    [reserved-terms (token lexeme lexeme)]
-   [alphabet (token 'IDENT (string->symbol lexeme))]))
+   [identifier (token 'IDENT (string->symbol lexeme))]))
 
 (provide lambda-calculus-lexer)
